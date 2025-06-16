3562a9c1a0e27b12a99eed604225357e99df9dca
larao <me@larao.dev>
2025-06-17 02:24:40 +0900

    add emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,65 @@
+;; Setup use-package
+(require 'package)
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+(setq package-archives
+      '(("elpa" . "https://elpa.gnu.org/packages/")
+	("melpa" . "https://melpa.org/packages/")))
+(package-initialize)
+
+(when (equal system-type 'darwin)
+  (setq mac-option-modifier 'meta))
+
+(load-theme 'leuven t)
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (exec-path-from-shell-initialize))
+
+(use-package company
+  :config
+  (global-company-mode)
+  (push 'company-lsp company-backends))
+
+
+(use-package go-mode
+  :ensure t)
+
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (
+	 (go-mode . lsp)
+	 )
+  :commands lsp
+  :config
+  (setq lsp-auto-guess-root t)
+  (setq lsp-enable-file-watchers nil))
+
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package magit)
+
+(use-package vterm)
+
+(use-package projectile
+  :config
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
