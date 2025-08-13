59454a8aa55608e2da4a853c9283729ac46dd9fc
larao <larao@pixiv.co.jp>
2025-08-13 22:07:51 +0900

    update neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -48,6 +48,7 @@ vim.api.nvim_create_user_command("DppUpdate", function(opts)
 	vim.fn["dpp#async_ext_action"]("installer", "update", { names = args })
 end, { nargs = "*" })
 
+vim.o.cursorline = true
 vim.o.clipboard = "unnamedplus"
 vim.o.smartindent = true
 vim.o.ignorecase = true
