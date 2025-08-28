dcbb0850bda0c81805a0f81a6be1cacb16a2eeb1
larao <larao@pixiv.co.jp>
2025-08-29 04:34:20 +0900

    update neovim config

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -95,6 +95,15 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
+          require("none-ls.formatting.eslint_d"),
+          require("none-ls.diagnostics.eslint_d"),
+          require("none-ls.code_actions.eslint_d"),
+          null_ls.builtins.formatting.prettierd.with({
+            filetypes = {
+              "json",
+              "yaml",
+            },
+          }),
           null_ls.builtins.diagnostics.phpstan.with({
             -- timeoutはdefaultで5sで設定されていてremote環境ではtimeoutしてしまっていたため10sに伸ばした
             timeout = 10000,
