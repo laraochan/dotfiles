a580e537c2e86dcdfc4030ccec8bf06149027a31
larao <larao@pixiv.co.jp>
2025-09-02 20:29:05 +0900

    update yabai + skhd config

---
+++ yabai/display_fix.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+yabai -m space 1 --display 1
+yabai -m space 2 --display 1
+yabai -m space 3 --display 1
+yabai -m space 4 --display 1
+yabai -m space 5 --display 1
+yabai -m space 6 --display 2
+yabai -m space 7 --display 2
+yabai -m space 8 --display 2
+yabai -m space 9 --display 2
+yabai -m space 10 --display 2
