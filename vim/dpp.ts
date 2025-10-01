5c81ca4a79ee9fd96a0d9e97f6e35fed4c9feca6
laraochan <me@larao.dev>
2025-10-02 02:55:58 +0900

    add vim config

---
+++ vim/dpp.ts
@@ -0,0 +1,41 @@
+import {
+	BaseConfig,
+	type ConfigArguments,
+	type ConfigReturn,
+} from "jsr:@shougo/dpp-vim@5.0.0/config";
+import { type Toml } from "jsr:@shougo/dpp-ext-toml@2.0.1";
+import { expand } from "jsr:@denops/std@8.0.0/function";
+
+
+export class Config extends BaseConfig {
+	override async config(args: ConfigArguments): Promise<ConfigReturn> {
+		args.contextBuilder.setGlobal({
+			protocols: ["git"],
+		});
+
+		const [context, options] = await args.contextBuilder.get(args.denops);
+		const toml: Toml = await args.dpp.extAction(
+			args.denops,
+			context,
+			options,
+			"toml",
+			"load",
+			{
+				path: await expand(args.denops, "~/.config/vim/plugins.toml")
+			}
+		);
+
+		const tomlHooksFiles: string[] | undefined = [];
+		if (toml.hooks_file !== undefined) {
+			tomlHooksFiles.push(toml.hooks_file)
+		}
+
+		return {
+			plugins: toml.plugins ?? [],
+			ftplugins: toml.ftplugins,
+			hooksFiles: tomlHooksFiles,
+			multipleHooks: toml.multiple_hooks,
+		}
+	}
+}
+
