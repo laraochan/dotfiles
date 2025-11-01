97776b85d14d63e2d6007cf08cd8a6cc30f05c0f
laraochan <me@larao.dev>
2025-11-02 01:58:58 +0900

    add neovim config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,23 @@
+-- Put this at the top of 'init.lua'
+local path_package = vim.fn.stdpath('data') .. '/site'
+local mini_path = path_package .. '/pack/deps/start/mini.nvim'
+if not vim.loop.fs_stat(mini_path) then
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
+  local clone_cmd = {
+    'git', 'clone', '--filter=blob:none',
+    -- Uncomment next line to use 'stable' branch
+    -- '--branch', 'stable',
+    'https://github.com/nvim-mini/mini.nvim', mini_path
+  }
+  vim.fn.system(clone_cmd)
+  vim.cmd('packadd mini.nvim | helptags ALL')
+  vim.cmd('echo "Installed `mini.nvim`" | redraw')
+end
+
+require("mini.deps").setup({ path = { package = path_package } })
+local add = MiniDeps.add
+
+vim.o.termguicolors = true
+vim.o.cursorline = true
+
+add("nvim-mini/mini.comment")
