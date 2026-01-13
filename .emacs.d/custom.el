2f0941f83c90f6efd0bc3a33ad1875fae25cd9a2
laraochan <me@larao.dev>
2026-01-13 21:42:54 +0900

    update config

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,14 +4,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("3061706fa92759264751c64950df09b285e3a2d3a9db771e99bcbb2f9b470037"
-     "7771c8496c10162220af0ca7b7e61459cb42d18c35ce272a63461c0fc1336015"
-     "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
-     "e8ceeba381ba723b59a9abc4961f41583112fc7dc0e886d9fc36fa1dc37b4079"
-     "456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641"
-     "9e5e0ff3a81344c9b1e6bfc9b3dcf9b96d5ec6a60d8de6d4c762ee9e2121dfb2"
-     default)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
