afacf846a5949fbdb3693c8f336dfc15f9c91e93
laraochan <me@larao.dev>
2026-02-01 18:02:51 +0900

    update config

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -11,109 +11,93 @@
 
   (leaf leaf-keywords
     :ensure t
-    :init
-    ;; optional packages if you want to use :hydra, :el-get, :blackout,,,
-    (leaf hydra :ensure t)
-    (leaf el-get :ensure t)
-    (leaf blackout :ensure t)
-
     :config
     ;; initialize leaf-keywords.el
     (leaf-keywords-init)))
 
-(leaf cus-edit
-  :doc "tools for customizing Emacs and Lisp packages"
-  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
-
-(leaf cus-start
-  :custom '((user-full-name . "Sora Terao")
-	    (user-mail-address . "me@larao.dev")
-	    (user-login-name . "laraochan")
-	    (create-lockfiles . nil)
-	    (tab-width . 4)
-	    (debug-on-error . t)
-	    (init-file-debug . t)
-	    (frame-resize-pixelwise . t)
-	    (enable-recursive-minibuffers . t)
-		(history-length . 1000)
-		(history-delete-duplicates . t)
-		(scroll-preserve-screen-position . t)
-		(scroll-conservatively . 100)
-		(mouse-wheel-scroll-amount . '(1 ((control) . 5)))
-		(ring-bell-function . 'ignore)
-		(text-quoting-style . 'straight)
-		(truncate-lines . t)
-		(use-dialog-box . nil)
-		(use-file-dialog . nil)
-		(menu-bar-mode . t)
-		(tool-bar-mode . nil)
-		(scroll-bar-mode . nil)
-		(indent-tabs-mode . nil)))
-
-(leaf autorevert
-  :doc "revert buffers when files on disk change"
-  :global-minor-mode global-auto-revert-mode)
-
-(leaf delsel
-  :doc "delete selection if you insert"
-  :global-minor-mode delete-selection-mode)
-
-(leaf paren
-  :doc "highlight matching paren"
-  :global-minor-mode show-paren-mode)
-
-(leaf savehist
-  :init (savehist-mode))
-
-(leaf which-key
-  :global-minor-mode t)
-
 (leaf doom-themes
   :ensure t
   :config
-  (load-theme 'doom-winter-is-coming-dark-blue t))
-
-(leaf doom-modeline
-  :global-minor-mode t
-  :ensure t)
-
-(leaf nyan-mode
-  :global-minor-mode t
-  :ensure t
-  :custom
-  (nyan-animate-nyancat . t))
-
-(leaf dashboard
-  :ensure t
-  :config
-  (dashboard-setup-startup-hook)
-  :custom
-  (dashboard-center-content . t))
+  (load-theme 'doom-moonlight t))
 
 (leaf vertico
-  :global-minor-mode t
-  :ensure t)
+  :ensure t
+  :global-minor-mode t)
 
 (leaf marginalia
-  :global-minor-mode t
+  :ensure t
+  :global-minor-mode t)
+
+(leaf treesit
+  :custom
+  (treesit-font-lock-level . 4)
+  :config
+  (leaf treesit-auto
+    :ensure t
+    :global-minor-mode t))
+
+(leaf typescript-ts-mode
+  :mode '(("\\.[mc]?ts\\'" . typescript-ts-mode)
+		  ("\\.tsx\\'" . tsx-ts-mode)))
+
+(leaf eglot
+  :hook ((typescript-ts-mode-hook . eglot-ensure)
+	 (tsx-ts-mode-hook . eglot-ensure)))
+
+(leaf magit
   :ensure t)
+
+(leaf vterm
+  :ensure t
+  :config
+  (leaf multi-vterm
+	:ensure t
+	:after vterm))
 
 (leaf nerd-icons
   :ensure t
   :config
   (leaf nerd-icons-dired
     :ensure t
+    :after dired
     :hook (dired-mode-hook . nerd-icons-dired-mode))
   (leaf nerd-icons-ibuffer
     :ensure t
+    :after ibuffer
     :hook (ibuffer-mode-hook . nerd-icons-ibuffer-mode))
   (leaf nerd-icons-completion
-    :ensure t)
+    :ensure t
+    :after marginalia
+    :global-minor-mode t
+    :hook (marginalia-mode-hook . #'nerd-icons-completion-marginalia-setup))
   (leaf nerd-icons-corfu
     :ensure t
     :after corfu
     :config
     (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter)))
+
+
+(leaf dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  :custom
+  (dashboard-startup-banner . "~/.emacs.d/assets/127051174_p0.jpg")
+  (dashboard-image-banner-max-width . 300)
+  (dashboard-image-banner-max-height . 300)
+  (dashboard-center-content . t)
+  (dashboard-display-icons-p . t)
+  (dashboard-icon-type . 'nerd-icons))
+
+(leaf doom-modeline
+  :ensure t
+  :global-minor-mode t)
+
+(leaf nyan-mode
+  :ensure t
+  :global-minor-mode t
+  :custom
+  (nyan-animate-nyancat . t))
 
 (leaf corfu
   :ensure t
@@ -127,68 +111,94 @@
   (corfu-quit-no-match . t)
   (corfu-quit-at-boundary . t))
 
-(leaf cape
-  :ensure t
-  :config
-  (add-to-list 'completion-at-point-functions #'cape-file))
-
-(leaf orderless
-  :ensure t
-  :custom
-  (completion-styles . '(orderless basic))
-  (completion-category-defaults . nil)
-  (completion-category-overrides . '((file (styles partial-completion))))
-  (completion-pcm-leading-wildcard . t)) ;; Emacs 31: partial-completion behaves like substring
-
-(leaf treesit
-  :custom
-  (treesit-font-lock-level . 4)
-  (treesit-language-source-alist . '((json "https://github.com/tree-sitter/tree-sitter-json")
-				     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-				     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-				     (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
-				     (html "https://github.com/tree-sitter/tree-sitter-html")
-				     (css "https://github.com/tree-sitter/tree-sitter-css")
-				     (php "https://github.com/tree-sitter/tree-sitter-php" "master" "php/src")
-				     (phpdoc "https://github.com/claytonrcarter/tree-sitter-phpdoc")
-				     (markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src")))
-  :config
-  (dolist (element treesit-language-source-alist)
-    (let* ((lang (car element)))
-      (if (treesit-language-available-p lang)
-	  (message "treesit: %s is already installed" lang)
-	(message "treesit: %s is not installed" lang)
-	(treesit-install-language-grammar lang)))))
-
-(leaf typescript-ts-mode
-  :mode (("\\.[m]?ts\\'" . typescript-ts-mode)
-	 ("\\.tsx\\'" . tsx-ts-mode)))
-
-(leaf js-ts-mode
-  :mode ("\\.[mc]?js\\'" . js-ts-mode))
-
-(leaf json-ts-mode
-  :mode ("\\.json\\'" . json-ts-mode))
-
-(leaf toml-ts-mode
-  :mode ("\\.md\\'" . markdown-ts-mode))
-
-(leaf php-ts-mode
-  :mode ("\\.php\\'" . php-ts-mode))
-
-(leaf eglot
-  :hook ((tsx-ts-mode-hook . eglot-ensure)
-	 (typescript-ts-mode-hook . eglot-ensure)
-	 (php-ts-mode-hook . eglot-ensure)))
-
-(leaf eglot-booster
-  :when (executable-find "emacs-lsp-booster")
-  :vc (:url "https://github.com/jdtsmith/eglot-booster")
+(leaf which-key
   :global-minor-mode t)
 
-(leaf magit
+(leaf cus-edit
+  :doc "tools for customizing Emacs and Lisp packages"
+  :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
+
+(leaf cus-start
+  :custom
+  (user-full-name . "Sora Terao")
+  (user-mail-address . "me@larao.dev")
+  (user-login-name . "laraochan")
+  (create-lockfiles . nil)
+  (tab-width . 4)
+  (indent-tab-mode . nil)
+  (debug-on-error . t)
+  (init-file-debug . t)
+  (frame-resize-pixelwise . t)
+  (enable-recursive-minibuffers . t)
+  (history-length . 1000)
+  (history-delete-deplicates . t)
+  (scroll-preserve-screen-position . t)
+  (scroll-conservatively . 100)
+  (mouse-wheel-scroll-amount . '(1 ((control) . 5)))
+  (ring-bell-function . 'ignore)
+  (text-quoting-style . 'straight)
+  (truncate-lines . t)
+  (use-dialog-box . nil)
+  (use-file-dialog . nil)
+  (menu-bar-mode . t)
+  (tool-bar-mode . t)
+  (scroll-bar-mode . nil))
+
+(leaf autorevert
+  :doc "revert buffers when files on disk change"
+  :global-minor-mode global-auto-revert-mode)
+
+(leaf delsel
+  :doc "delete selection if you insert"
+  :global-minor-mode delete-selection-mode)
+
+(leaf paren
+  :doc "highlight matching paren"
+  :global-minor-mode show-paren-mode)
+
+(leaf simple
+  :doc "basic editing commands for Emacs"
+  :custom ((kill-read-only-ok . t)
+           (kill-whole-line . t)
+           (eval-expression-print-length . nil)
+           (eval-expression-print-level . nil)))
+
+(leaf files
+  :doc "file input and output commands for Emacs"
+  :global-minor-mode auto-save-visited-mode
+  :custom `((auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
+            (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
+                                        (,tramp-file-name-regexp . nil)))
+            (version-control . t)
+            (delete-old-versions . t)
+            (auto-save-visited-interval . 1)))
+
+(leaf startup
+  :doc "process Emacs shell arguments"
+  :custom `((auto-save-list-file-prefix . ,(locate-user-emacs-file "backup/.saves-"))))
+
+(leaf savehist
+  :doc "Save minibuffer history"
+  :custom `((savehist-file . ,(locate-user-emacs-file "savehist")))
+  :global-minor-mode t)
+
+(leaf flymake
+  :doc "A universal on-the-fly syntax checker"
+  :bind ((prog-mode-map
+          ("M-n" . flymake-goto-next-error)
+          ("M-p" . flymake-goto-prev-error)))
+  :config
+  (leaf flymake-eslint
+    :ensure t)
+  (leaf phpstan
+    :ensure t))
+
+(leaf markdown-mode
   :ensure t)
 
-(leaf diff-hl
-  :global-minor-mode t
-  :ensure t)
+(leaf org
+  :config
+  (leaf org-modern
+	:ensure t
+	:hook ((org-mode-hook . org-modern-mode)
+		   (org-agenda-finalize-hook . org-modern-mode))))
