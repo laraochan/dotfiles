b46e08e8f84cc09d9a8b152bb33010f747863d5d
laraochan <me@larao.dev>
2026-02-02 21:05:55 +0900

    update config

--- dot_config/nvim/after/ftplugin/php.lua
+++
@@ -1,5 +0,0 @@
-local tabwidth = 4
-vim.opt.expandtab = true
-vim.opt.tabstop = tabwidth
-vim.opt.softtabstop = tabwidth
-vim.opt.shiftwidth = tabwidth
--- dot_config/wezterm/wezterm.lua
+++
@@ -1,7 +0,0 @@
-local wezterm = require("wezterm")
-local config = wezterm.config_builder()
-
-config.color_scheme = 'Kanagawa (Gogh)'
-config.ime_preedit_rendering = 'Builtin'
-
-return config
